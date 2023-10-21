adc988c642ab55b12c3ae9b6a0815077c7259907
mana8100 <emanacorda@outlook.com>
2023-10-21 15:31:11 -0400

    Added TextProvider Interface and Implementation.

---
+++ src/Meliora/Counter/TextProviders/ITextProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Counter.TextProviders;
+
+public interface ITextProvider
+{
+    string GetText<T>( T number );
+}
+
+public class TextProviderInvariantCulture : ITextProvider
+{
+    public virtual string GetText<T>( T number )
+    {
+        // Set the default culture for conversions (e.g., parsing, formatting).
+        var currentCultureInfo = CultureInfo.DefaultThreadCurrentCulture;
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+        var text = number?.ToString() ?? "";
+
+        // Restore the default culture for conversions (e.g., parsing, formatting).
+        CultureInfo.DefaultThreadCurrentCulture = currentCultureInfo;
+
+        return text;
+    }
+}
---
+++ src/Meliora/CounterTest/TextProviders/TextProviderTest.cs
@@ -0,0 +1,54 @@
+﻿using Counter.TextProviders;
+
+namespace CounterTest.TextProviders;
+
+public class TextProviderTest
+{
+    [ Theory ]
+    [ InlineData( 1, "1" ) ]
+    [ InlineData( 3, "3" ) ]
+    [ InlineData( -25, "-25" ) ]
+    public void GetText_Integer_Successfully( int number, string expected )
+    {
+        // Arrange
+        ITextProvider textProvider = new TextProviderInvariantCulture();
+
+        // Act
+        var result = textProvider.GetText( number );
+
+        // Assert
+        result.Should().Be( expected );
+    }
+
+    [ Theory ]
+    [ InlineData( 1.2, "1.2" ) ]
+    [ InlineData( -.5, "-0.5" ) ]
+    [ InlineData( 0, "0" ) ]
+    public void GetText_Double_Successfully( double number, string expected )
+    {
+        // Arrange
+        ITextProvider textProvider = new TextProviderInvariantCulture();
+
+        // Act
+        var result = textProvider.GetText( number );
+
+        // Assert
+        result.Should().Be( expected );
+    }
+
+    [ Theory ]
+    [ InlineData( "Hello", "Hello" ) ]
+    [ InlineData( "", "" ) ]
+    [ InlineData( null, "" ) ]
+    public void GetText_String_Successfully( string? number, string expected )
+    {
+        // Arrange
+        ITextProvider textProvider = new TextProviderInvariantCulture();
+
+        // Act
+        var result = textProvider.GetText( number );
+
+        // Assert
+        result.Should().Be( expected );
+    }
+}
