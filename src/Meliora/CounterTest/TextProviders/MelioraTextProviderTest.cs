e6a453ac5ff1625218d72d6ad1ae19cb1f6bc359
mana8100 <emanacorda@outlook.com>
2023-10-21 17:01:33 -0400

    Added TextProvider for Meliora.

---
+++ src/Meliora/CounterTest/TextProviders/MelioraTextProviderTest.cs
@@ -0,0 +1,27 @@
+﻿using Counter.Numbers;
+using Counter.TextProviders;
+
+namespace CounterTest.TextProviders;
+
+public class MelioraTextProviderTest
+{
+    [ Theory ]
+    [ InlineData( 1, "1" ) ]
+    [ InlineData( 6, "Nursing" ) ]
+    [ InlineData( 14, "Meliora" ) ]
+    [ InlineData( 21, "Nursing Meliora" ) ]
+    [ InlineData( 41, "41" ) ]
+    [ InlineData( -42, "Nursing Meliora" ) ]
+    public void GetText_Successfully( int number, string expected )
+    {
+        // Arrange
+        IMultipleChecker multipleChecker = new MultipleChecker();
+        ITextProvider<int> textProvider = new MelioraTextProvider( multipleChecker );
+
+        // Act
+        var result = textProvider.GetText( number );
+
+        // Assert
+        result.Should().Be( expected );
+    }
+}
